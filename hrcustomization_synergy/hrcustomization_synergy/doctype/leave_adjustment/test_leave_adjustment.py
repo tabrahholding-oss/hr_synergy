3f701bcd754daa6a7433a996e28eb2144b113e6b
saqlain-zahid-6468 <saqlainzahid6468@gmail.com>
2026-06-02 23:26:23 +0500

    added leave adjustment doctype

---
+++ hrcustomization_synergy/hrcustomization_synergy/doctype/leave_adjustment/test_leave_adjustment.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Aadhil and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLeaveAdjustment(FrappeTestCase):
+	pass
