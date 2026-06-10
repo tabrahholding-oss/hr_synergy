9cebe2eb857b76775fdd12f109fefb2142f11e30
saqlain-zahid-6468 <saqlainzahid6468@gmail.com>
2026-06-10 19:39:13 +0500

    refactor: remove Stock Entry Summary Report files

--- hrcustomization_synergy/hrcustomization_synergy/report/stock_entry_summary_report/stock_entry_summary_report.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, NexTash and contributors
-// For license information, please see license.txt
-
-frappe.query_reports["Stock Entry Summary Report"] = {
-	"filters": [
-
-	]
-};
