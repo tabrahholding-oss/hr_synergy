c23bd879d3ba5f7f1f82efac9143c6ed717a912b
saqlain-zahid-6468 <saqlainzahid6468@gmail.com>
2026-07-10 16:18:44 +0500

    feat: Add Certificate Request Detail doctype with associated JS, JSON, and print format files

---
+++ hrcustomization_synergy/hrcustomization_synergy/doctype/certificate_request_detail/test_certificate_request_detail.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, NexTash and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestCertificateRequestDetail(FrappeTestCase):
+	pass
